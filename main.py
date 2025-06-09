a0337e07705621c68821b2eaa0b0b91ede8ad27f
Gabriel-2511 <165502792+Gabriel-2511@users.noreply.github.com>
2025-06-08 20:20:18 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como', bot.user)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot, {bot.user}!')
+
+@bot.command()
+async def color(ctx):
+    await ctx.send('Mi color favorito es el azul')
+
+@bot.command()
+async def passw(ctx):
+    await ctx.send ("Contraseña:" + gen_pass(10))
+    
+bot.run("Token")
